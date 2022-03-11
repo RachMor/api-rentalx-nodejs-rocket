3da802210308d34a5bb70b96bac47672a9cf45c1
Raquel Moreira <raquel.moreira@nextpage.com.br>
2022-03-11 16:48:44 -0300

    docker and typeorm

--- prettier.config.js
+++ prettier.config.js
@@ -1,3 +1,18 @@
 module.exports = {
-  singleQuote: false;
+  tabWidth: 2,
+  useTabs: false,
+  endOfLine: 'lf',
+  semi: false,
+  singleQuote: true,
+  printWidth: 100,
+  trailingComma: 'es6',
+  bracketSpacing: true,
+  overrides: [
+    {
+      files: '*.js',
+      options: {
+        parser: 'flow',
+      },
+    },
+  ],
 }
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express from 'express';
+import swagger from 'swagger-ui-express';
 
 import { routes } from './routes';
+import swaggerFile from './swagger.json';
+
+import './database';
 
 const app = express();
 
 app.use(express.json());
+app.use('/api-docs', swagger.serve, swagger.setup(swaggerFile));
 
 app.use(routes);
 
